a5acb25a17632b1fca81cd98df991d2c1e713791
21pages <2549299455@qq.com>
2020-03-17 17:48:27 +0800

    first commit

---
+++ build_gen.bat
@@ -0,0 +1,21 @@
+@if exist build ( rmdir /q /s build )
+@mkdir build
+@cd build
+
+@rem 设置渲染模式
+@set NANOVG_TYPE=AGGE
+@rem set NANOVG_TYPE=GLES2
+@rem set NANOVG_TYPE=GLES3
+@rem set NANOVG_TYPE=AGG
+
+@rem 设置启用谷歌拼音
+@rem PINYIN_ENABLE=ON
+
+@rem 禁用部分警告
+@set DISABLE_WARNING=ON
+
+@rem 设置VS生成器
+@set VS_GENERATOR="Visual Studio 15 Win64"
+
+D:\program\cmake\bin\cmake -DNANOVG_BACKEND=%NANOVG_TYPE% -DGPINYIN_ENABLE=%PINYIN_ENABLE% -DDISABLE_WARNING=%DISABLE_WARNING% -G %VS_GENERATOR% ../cmake
+pause
